b9eca62a01905c809bcd52093ed7d14b78fd1267
Jeff Mikels <jeffweb@mikels.cc>
2013-08-19 16:55:39 -0400

    added styles

--- js/main.js
+++ js/main.js
@@ -8,26 +8,17 @@ var app = {
 				}
 		},
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
+
+
+
 
     initialize: function() {
   		  var self = this;
         this.store = new MemoryStore(function()
         {
-        	self.showAlert('Store Initialized', 'Info');
+        	//self.showAlert('Store Initialized', 'Info');
+        	$('body').html(new HomeView(self.store).render().el);
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
 };
